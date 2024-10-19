f9b3f7eb9f260c98cf151c32faccbeb0ac6e8d16
lualvare <diego198709@gmail.com>
2024-10-18 18:23:32 -0600

    Update powershell.ps1

--- modules/powershell.ps1
+++ modules/powershell.ps1
@@ -1,1 +1,10 @@
 Install-WindowsFeature -Name DNS -IncludeManagementTools
+$DnsServerSettings = Get-DnsServerSetting -All
+$NameServerIPaddr = (Get-DnsServerSetting).ListeningIPAddress.IPAddressToString | Select-String "^10"
+$DnsServerSettings.ListeningIpAddress = @("$NameServerIPaddr")
+Set-DnsServerSetting $DnsServerSettings
+Add-DnsServerPrimaryZone -Name "akslab.com" -ZoneFile "akslab.com.dns"
+Add-DnsServerPrimaryZone -NetworkId "10.1.0.0/24" -ZoneFile "0.1.10.in-addr.arpa.dns"
+Add-DnsServerResourceRecordA -Name "www" -ZoneName "akslab.com" -IPv4Address "10.1.0.100" -CreatePtr
+$Forwarders = @("8.8.8.8", "8.8.4.4", "168.63.129.16")
+Set-DnsServerForwarder -IPAddress $Forwarders
